fe05c169714feeb3232f3ca484b82591ec2a45eb
aly_dev07 <aly_dev07@yahoo.com>
2024-01-02 13:49:38 +1300

    folder for each day creating script added.

--- ipcamera.py
+++ ipcamera.py
@@ -1,17 +1,25 @@
 import subprocess
 import time
+import os
 
 # Replace with your RTSP stream URL
-rtsp_url = "rtsp://path-to-onvif-stream"
+rtsp_url = "rtsp://784352:888888@192.168.20.16:554/onvif1"
 
 # Set image capture interval (in seconds)
 capture_interval = 30
 
 # Path to save the captured images
-image_path = "images/"
+image_path = "images/" + time.strftime('%Y-%m-%d')
+
+
+if not os.path.exists(image_path):
+    os.mkdir(image_path)
+    
 
 while True:
     try:
+        image_name = "image_" + time.strftime('%H.%M.%S')
+        
         # FFmpeg command to capture a single image
         ffmpeg_command = [
             "ffmpeg",
@@ -20,7 +28,7 @@ while True:
             "-ss", "00:00:01",
             "-vframes", "1",  # Capture only one frame
             "-f", "image2",  # Output format as image2
-            f"{image_path}/image_{int(time.time())}.jpg"
+            f"{image_path}/{image_name}.jpg"
         ]
 
         # Execute the FFmpeg command
